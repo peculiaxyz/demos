24f2f996dda3be05402eb90ada9f530ae43afd9c
Ndamulelo Nemakhavhani <ndamulelo@bbd.co.za>
2021-01-09 08:39:11 +0200

    add twilio callback urls

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", method='POST')
 def index():
     return 'This is a demo whatsapp bot api'
+
+@app.route("/status", method='POST')
+def index():
+    return 'Delivery status for message, sent to = TBC'
 
 
 # Press the green button in the gutter to run the script.
