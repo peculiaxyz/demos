6b69259e7260b32a262567a80eccfa77649f3b7d
endeesa <endeesa@bbd.extra>
2021-01-09 09:27:33 +0200

    add twilio repose struct

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,31 @@
-from flask import Flask
+import flask
+from twilio.twiml.messaging_response import MessagingResponse
+import logging
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
+
+def _build_twilio_response(message:str, media_url:str=None):
+    response = MessagingResponse()
+    message = response.message()
+    message.body(message)
+    if media_url not in (None, ''):
+        message.media(media_url)
+    return str(response.to_xml())
 
 @app.route("/", methods=['POST'])
 def incoming_message_callback():
-    return 'This is a demo whatsapp bot api'
+    incoming_msg = flask.request.values.get('Body')  # TODO: ask user to try again here
+    logger.info(f'Incoming message from twilio: {incoming_msg}')
+    response = _build_twilio_response('This is a demo whatsapp bot api')
+    return response
 
 @app.route("/status", methods=['POST'])
 def delivery_status_callback():
-    return 'Delivery status for message, sent to = TBC'
+    return _build_twilio_response('Delivery status callback ready')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run()
