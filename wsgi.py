e88cbee69d58694353d85005e6876900a9025595
endeesa <endeesa@bbd.extra>
2021-01-09 08:56:25 +0200

    fix routes

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", method='POST')
+@app.route("/", methods=['POST'])
 def index():
     return 'This is a demo whatsapp bot api'
 
-@app.route("/status", method='POST')
+@app.route("/status", methods=['POST'])
 def index():
     return 'Delivery status for message, sent to = TBC'
 
