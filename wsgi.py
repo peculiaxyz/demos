538890f49006c364264bff74ef6057d3a5e907d0
endeesa <endeesa@bbd.extra>
2021-01-09 14:23:31 +0200

    integrate tensorflow to api

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 import flask
+import _dialog_engines as eng
 from twilio.twiml.messaging_response import MessagingResponse
 import logging
 
@@ -15,11 +16,27 @@ def _build_twilio_response(message:str, media_url:str=None):
         msg.media(media_url)
     return str(response)
 
+def _sanitize_sender_info(sender:str):
+    if 'whatsapp' in sender:
+        sender = sender.replace('whatsapp', '')
+    if 'sms' in sender:
+        sender = sender.replace('whatsapp', '')
+    return sender
+
 @app.route("/", methods=['POST'])
 def incoming_message_callback():
-    incoming_msg = flask.request.values.get('Body')  # TODO: ask user to try again here
+    incoming_msg = flask.request.values.get('Body')
+    sender = flask.request.values.get('From')
+    sender = _sanitize_sender_info(sender)
     logger.info(f'Incoming message from twilio: {incoming_msg}')
-    response = _build_twilio_response('This is a demo whatsapp bot api')
+
+    input = eng.DialogEngineInput()
+    input.input_message = incoming_msg
+    input.sender = sender
+    engine = eng.TensoFlowDialogEngine(request_data=input)
+    botresponse = engine.execute()
+
+    response = _build_twilio_response(botresponse)
     return response
 
 @app.route("/status", methods=['POST'])
